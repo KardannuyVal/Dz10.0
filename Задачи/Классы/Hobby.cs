44c567c3a58ab2fa24b6798d6d7ec215016085a4
Кардан <143964266+KardannuyVal@users.noreply.github.com>
2023-12-01 18:48:48 +0300

    Update Hobby.cs

--- Задачи/Классы/Hobby.cs
+++ Задачи/Классы/Hobby.cs
@@ -5,17 +5,7 @@ namespace Задачи
 {
     internal class Hobby
     {
-        /// <summary>
-        /// Делегат
-        /// </summary>
-        /// <param name="hobby"></param>
-        /// <param name="person"></param>
         public delegate void HobbyDelegate(Hobbys hobby, List<Person> person);
-        /// <summary>
-        /// Метод, который показывает реакцию человека на событие
-        /// </summary>
-        /// <param name="hobby"></param>
-        /// <param name="person"></param>
         public static void Reaction(Hobbys hobby, List<Person> person)
         {
             for (int i = 0; person.Count > i; i++)
